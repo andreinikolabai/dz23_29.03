f2664a5a5f2cf4fafc850062a28586347cbfa1ba
andreinikolabai <andreiplus2000@gmail.com>
2023-03-30 23:49:22 +0300

    update

--- main.js
+++ main.js
@@ -1,26 +1,23 @@
 const count = 10;
 let array = new Array(count);
 
-fillMatrix();
-console.log(array);
-showMatrix();
-
 function fillMatrix() {
-    for (let i = 0; i < count; ++i) {
+    for (let i = 0; i < count; i++) {
         array[i] = new Array(count);
-        for (let j = 0; j < count; ++j) {
+        for (let j = 0; j < count; j++) {
             array[i][j] = i * 10 + j + 1;
         }
     }
 }
 function showMatrix() {
-    for (let i = 0; i < count; ++i) {
-        for (let j = 0; j < count; ++j) {
-            // Вывести с выравниванием
-            document.write(array[i][j].toString().padStart(4,"\u00A0"));
-            // Если выравнивание не интересует, то:
-            // document.write(array[i][j] +" ");
+    for (let i = 0; i < count; i++) {
+        for (let j = 0; j < count; j++) {
+            document.write(array[i][j].toString().padStart(10,"\u00A0"));
         }
-        document.write("<br />");
+        document.write("<br/>");
     }
 }
+
+fillMatrix();
+showMatrix();
+console.log(array);
